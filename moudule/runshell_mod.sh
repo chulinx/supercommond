750403b06030ac2527a3a64bf4f6c715181196e8
zhangxiang <zhangxiang@mitake.com.cn>
2016-12-29 15:51:31 +0800

    work ok

--- moudule/runshell_mod.sh
+++ moudule/runshell_mod.sh
@@ -11,9 +11,13 @@ fi
 
 irunshell()
 {
-scp $3 $i:$4
-ssh -t $i chown mds:mds $4 && chmod 774 $4
-ssh $i bash -x $4
+echo -e  ${RED_COLOR}"正在复制shell文件$2"${RES}
+scp $3 $i:$4             
+ssh -t $i chown mds:mds $4 >>$LogDir/supercommond.log 2>&1
+ssh -t  chmod 774 $4     >>$LogDir/supercommond.log 2>&1
+ScriptName=`echo $3 |awk -F '[/]' '{print $NF}'` >>$LogDir/supercommond.log 2>&1
+echo -e ${RED_COLOR}正在执行Scripts...${RES}
+ssh $i bash  $4$ScriptName
 }
 
 
@@ -21,9 +25,9 @@ hrunshell()
 {
 echo -e  ${RED_COLOR}"正在复制shell文件$2"${RES}
 scp $3 $2:$4
-ssh -t $2 chown mds:mds $4 >$DefaultDir/supercommond.log 2>&1
-ssh -t $2 chmod 774 $4 >$DefaultDir/supercommond.log 2>&1
-ScriptName=`echo $3 |awk -F '\/' '{print $NF}'` >$DefaultDir/supercommond.log 2>&1
+ssh -t $2 chown mds:mds $4 >>$LogDir/supercommond.log 2>&1
+ssh -t $2 chmod 774 $4 >>$TmpDir/supercommond.log 2>&1
+ScriptName=`echo $3 |awk -F '[/]' '{print $NF}'` >>$LogDir/supercommond.log 2>&1
 echo -e ${RED_COLOR}正在执行Scripts...${RES}
 ssh $2 bash  $4$ScriptName
 }
